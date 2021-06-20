72682076f17e17249bfbf8d1b9c4ec035a3e4e03
Ruslan O <realnaru@gmail.com>
2021-06-20 19:43:31 +0800

    delete second test project

---
+++ CustomerLibraryTests/IntegrationTests/CustomerNoteRepositoryTests.cs
@@ -0,0 +1,150 @@
+﻿using CustomerClassLibrary;
+using CustomerClassLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CustomerLibraryTests.IntegrationTests
+{
+    public class CustomerNoteRepositoryTests
+    {
+        [Fact]
+        public void ShouldbeAbleToCreateNoteRepository()
+        {
+            var customerNoteRepository = new CustomerNoteRepository();
+
+            Assert.NotNull(customerNoteRepository);
+        }
+
+        [Fact]
+        public void ShouldbeAbleToCreateCustomerNote()
+        {
+            var customerNoteRepository = new CustomerNoteRepository();
+            var customerRepository = new CustomerRepository();
+
+            customerRepository.DeleteAll();
+
+            var customerNote = new CustomerNote();
+            var fixture = new CustomerRepositoryFixture();
+            var customer = fixture.MockCustomer();
+            int customerId = customerRepository.Create(customer);
+
+            customerNote.CustomerId = customerId;
+            customerNote.Note = "Kitty ipsum dolor sit amet, shed everywhere shed everywhere";
+
+            int customerNoteId = customerNoteRepository.Create(customerNote);
+            Assert.NotEqual(0, customerNoteId);
+          
+        }
+
+        [Fact]
+        public void ShouldbeAbleToReadCustomerNote()
+        {
+            var customerNoteRepository = new CustomerNoteRepository();
+            var customerRepository = new CustomerRepository();
+            var fixture = new CustomerRepositoryFixture();
+
+            customerRepository.DeleteAll();
+
+            var customerNote = new CustomerNote();
+            
+            var customer = fixture.MockCustomer();
+            int customerId = customerRepository.Create(customer);
+
+            customerNote.CustomerId = customerId;
+            customerNote.Note = "Kitty ipsum dolor sit amet, shed everywhere shed everywhere";
+
+            customerNoteRepository.Create(customerNote);
+
+            var createdNote = customerNoteRepository.Read(customerNote.CustomerId);
+
+            Assert.NotNull(createdNote);
+            Assert.Equal(customerId, createdNote.CustomerId);
+            Assert.Equal("Kitty ipsum dolor sit amet, shed everywhere shed everywhere", createdNote.Note);
+        }
+
+        [Fact]
+        public void ShouldbeAbleToUpdateCustomerNote()
+        {
+            var customerNoteRepository = new CustomerNoteRepository();
+            var customerRepository = new CustomerRepository();
+            var fixture = new CustomerRepositoryFixture();
+
+            customerRepository.DeleteAll();
+
+            var customerNote = new CustomerNote();
+
+            var customer = fixture.MockCustomer();
+            int customerId = customerRepository.Create(customer);
+
+            customerNote.CustomerId = customerId;
+            customerNote.Note = "Kitty ipsum dolor sit amet, shed everywhere shed everywhere";
+
+            customerNoteRepository.Create(customerNote);
+
+            customerNote.Note = "Purr jum eat the grass rip the couch scratched sumbathe, shed everywhere rip the couch sleep in the sink fluffy fur canip scratched";
+            customerNoteRepository.Update(customerNote);
+
+            var createdNote = customerNoteRepository.Read(customerNote.CustomerId);
+
+            Assert.NotNull(createdNote);
+            Assert.Equal(customerId, createdNote.CustomerId);
+            Assert.Equal("Purr jum eat the grass rip the couch scratched sumbathe, shed everywhere rip the couch sleep in the sink fluffy fur canip scratched", createdNote.Note);
+        }
+
+        [Fact]
+        public void ShouldbeAbleToDeleteCustomerNote()
+        {
+            var customerNoteRepository = new CustomerNoteRepository();
+            var customerRepository = new CustomerRepository();
+            var fixture = new CustomerRepositoryFixture();
+
+            customerRepository.DeleteAll();
+
+            var customerNote = new CustomerNote();
+
+            var customer = fixture.MockCustomer();
+            int customerId = customerRepository.Create(customer);
+
+            customerNote.CustomerId = customerId;
+            customerNote.Note = "Kitty ipsum dolor sit amet, shed everywhere shed everywhere";
+
+            customerNoteRepository.Create(customerNote);
+
+            customerNoteRepository.Delete(customerId);
+
+            var deletedNote = customerNoteRepository.Read(customerNote.CustomerId);
+
+            Assert.Null(deletedNote);
+        }
+
+        [Fact]
+        public void ShouldToGetNotesByCustomerId()
+        {
+            var customerNoteRepository = new CustomerNoteRepository();
+            var customerRepository = new CustomerRepository();
+            var fixture = new CustomerRepositoryFixture();
+
+            customerRepository.DeleteAll();
+
+            var customerNote = new CustomerNote();
+
+            var customer = fixture.MockCustomer();
+            int customerId = customerRepository.Create(customer);
+
+            customerNote.CustomerId = customerId;
+            customerNote.Note = "Kitty ipsum dolor sit amet, shed everywhere shed everywhere";
+
+            customerNoteRepository.Create(customerNote);
+
+            List<CustomerNote> Notes = customerNoteRepository.ReadNoteByCutomerId(customerId);
+
+            var createdNote = Notes[0];
+            Assert.Equal("Kitty ipsum dolor sit amet, shed everywhere shed everywhere", createdNote.Note);
+
+        }
+    }
+}
---
+++ CustomerLibraryTests/IntegrationTests/CustomerRepositoryFixture.cs
@@ -0,0 +1,44 @@
+﻿using CustomerClassLibrary;
+using CustomerClassLibrary.Data;
+using CustomerLibrary.IntegrationTests.IntegrationTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerLibraryTests.IntegrationTests
+{
+    public class CustomerRepositoryFixture
+    {
+        public Customer CreateMockCustomer()
+        {
+            var customerRepository = new CustomerRepository();
+            customerRepository.DeleteAll();
+
+            var customer = MockCustomer();
+
+            customerRepository.Create(customer);
+
+            return customer;     
+        }
+
+        public Customer MockCustomer()
+        {
+            var fixtureAddress = new CustomerAddressFixture();
+
+            var address = fixtureAddress.MockAddress();
+
+            var customer = new Customer();
+            customer.FirstName = "John";
+            customer.LastName = "Doe";
+            customer.PhoneNumber = "1111111";
+            customer.Email = "jd@gmail.com";
+            customer.TotalPurshasesAmount = 100;
+            customer.AddAddress(address);
+            customer.AddNote(new CustomerNote());
+
+            return customer;
+        }
+    }
+}
